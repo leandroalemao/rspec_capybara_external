8849f21f971cff60348e2f1a8cfea5acd75e752c
Leandro Alemao <leandro.costantini@gmail.com>
2015-04-05 18:58:32 +0100

    Add min/max prices

--- spec/bitcoin_spec.rb
+++ spec/bitcoin_spec.rb
@@ -8,8 +8,10 @@ feature "Bitcoin Exchange Rate Search" do
 			within("#market-table") do
 				all(:xpath, "//table/tbody/tr").each do |a|
 					if a.text.match(/^Foxbit/)
+						binding.pry
 						@foxbit = a.text.split(" ")[0]
-						@foxbit_value = a.text.split(" ")[-2]
+						@foxbit_min_value = a.text.split(" ")[-2]
+						@foxbit_max_value = format("R$%.2f",((a.text.split(" ")[1].gsub(/[^\d\.]/, '').to_f / 1.0124) / 1.0124).round(2))
 					end
 				end
 			end
@@ -18,7 +20,8 @@ feature "Bitcoin Exchange Rate Search" do
 			fill_in "LoginDialog_passwordInput", with: ENV['SPEEDY_PWD']
 			find("#LoginDialog_cmdLogin").click
 			@bitcoin_price = find("#base_currentprice").text
-			@cotacao = (@foxbit_value.gsub(/[^\d\.]/, '').to_f / @bitcoin_price.to_f).round(4)
+			@min_cotacao = (@foxbit_min_value.gsub(/[^\d\.]/, '').to_f / @bitcoin_price.to_f).round(4)
+			@max_cotacao = (@foxbit_max_value.gsub(/[^\d\.]/, '').to_f / @bitcoin_price.to_f).round(4)
 			@time = Time.now.strftime("%d/%m/%Y %H:%M")
 	  end
 	end
@@ -31,7 +34,7 @@ feature "Bitcoin Exchange Rate Search" do
 		fill_in "from", with: "bitcoin@leandroalemao.co.uk"
 		fill_in "to", with: "leandro.costantini@gmail.com"
 		fill_in "subject", with: "Bitcoin Ticker: " + @time
-		@email_text = [@foxbit, @foxbit_value, @bitcoin_price, @cotacao].join(" ")
+		@email_text = [@foxbit, @foxbit_min_value, @foxbit_max_value, @bitcoin_price, @min_cotacao, @min_cotacao].join(" ")
 		page.execute_script("$(tinyMCE.editors[0].setContent('#{@email_text}'))")
 		find_button("Send").click
 	end
